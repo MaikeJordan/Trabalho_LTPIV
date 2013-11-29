348cf755b7c71d7fad44934b859ad9852d5ea7f0
HERICK <HERICK@HERICK-PC.mshome.net>
2013-11-29 16:11:43 -0200

    Modificação  Classe Produto

---
+++ TrabalhoLTPIV/src/java/br/edu/ifnmg/tads/TrabalhoLTPIV/DoMainModel/Tipoproduto.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.tads.TrabalhoLTPIV.DoMainModel;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Mauro
+ */
+@Entity
+public class Tipoproduto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(name = "Nome", length = 255)
+    private String nome;
+    @Column(name = "Descricao",length = 255)
+    private String descricao;
+    
+    public Tipoproduto(String nome, String descricao){
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+    
+    public Tipoproduto(){
+        this.nome = "";
+        this.descricao = "";
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        hash = 67 * hash + Objects.hashCode(this.nome);
+        hash = 67 * hash + Objects.hashCode(this.descricao);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Tipoproduto other = (Tipoproduto) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.descricao, other.descricao)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Tipoproduto{" + "id=" + id + '}';
+    }
+
+    
+}
