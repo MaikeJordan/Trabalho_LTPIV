fa56c4bcac5877ac5c71dad69b4fca866211d853
Mauro <maurojanu@hotmail.com>
2013-11-29 17:16:20 -0200

    Criação da Interface IRepositorio

---
+++ TrabalhoLTPIV/src/java/br/edu/ifnmg/tads/TrabalhoLTPIV/DoMainModel/IRepositorio.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.tads.TrabalhoLTPIV.DoMainModel;
+
+import java.util.List;
+
+/**
+ *
+ * @author Mauro
+ */
+public interface IRepositorio<T> {
+    
+  
+    boolean Salvar(T obj);
+    boolean Apagar(T obj);
+    T Abrir(Long id);
+    List<T> Buscar(T obj);
+    
+}
+    
