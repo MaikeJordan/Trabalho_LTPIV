9e290d97c8fe3274b1941bc08e20378972d515f8
Mauro <Mauro@MuroSergio>
2013-12-01 12:02:56 -0200

    Modificação classe Estoques

--- TrabalhoLTPIV/src/java/br/edu/ifnmg/tads/TrabalhoLTPIV/DoMainModel/Estoques.java
+++ TrabalhoLTPIV/src/java/br/edu/ifnmg/tads/TrabalhoLTPIV/DoMainModel/Estoques.java
@@ -7,47 +7,39 @@
 package br.edu.ifnmg.tads.TrabalhoLTPIV.DoMainModel;
 
 import java.io.Serializable;
-import javax.persistence.Basic;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
- * @author Desktop
+ * @author Mauro
  */
 @Entity
-@Table(name = "estoques")
-@XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Estoques.findAll", query = "SELECT e FROM Estoques e"),
-    @NamedQuery(name = "Estoques.findByEstoqueID", query = "SELECT e FROM Estoques e WHERE e.estoqueID = :estoqueID"),
-    @NamedQuery(name = "Estoques.findByQuantidade", query = "SELECT e FROM Estoques e WHERE e.quantidade = :quantidade")})
+
 public class Estoques implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
     @Column(name = "EstoqueID")
     private Integer estoqueID;
-    @Basic(optional = false)
-    @NotNull
     @Column(name = "Quantidade")
     private int quantidade;
     @JoinColumn(name = "EstoqueID", referencedColumnName = "ProdutoID", insertable = false, updatable = false)
     @OneToOne(optional = false)
     private Produtos produtos;
 
+    
+    public Estoques(int quantidade, Produtos produtos){
+        this.quantidade = quantidade;
+        this.produtos = produtos;
+    }
+    
     public Estoques() {
+        this.quantidade = 0;
+        this.produtos = null;
     }
 
     public Estoques(Integer estoqueID) {
@@ -85,27 +77,41 @@ public class Estoques implements Serializable {
 
     @Override
     public int hashCode() {
-        int hash = 0;
-        hash += (estoqueID != null ? estoqueID.hashCode() : 0);
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.estoqueID);
+        hash = 53 * hash + this.quantidade;
+        hash = 53 * hash + Objects.hashCode(this.produtos);
         return hash;
     }
 
     @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Estoques)) {
+    public boolean equals(Object obj) {
+        if (obj == null) {
             return false;
         }
-        Estoques other = (Estoques) object;
-        if ((this.estoqueID == null && other.estoqueID != null) || (this.estoqueID != null && !this.estoqueID.equals(other.estoqueID))) {
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Estoques other = (Estoques) obj;
+        if (!Objects.equals(this.estoqueID, other.estoqueID)) {
+            return false;
+        }
+        if (this.quantidade != other.quantidade) {
+            return false;
+        }
+        if (!Objects.equals(this.produtos, other.produtos)) {
             return false;
         }
         return true;
     }
 
+    
+
     @Override
     public String toString() {
-        return "br.edu.ifnmg.tads.TrabalhoLTPIV.DoMainModel.Estoques[ estoqueID=" + estoqueID + " ]";
+        return "Estoques{" + "estoqueID=" + estoqueID + '}';
     }
+
+    
     
 }
