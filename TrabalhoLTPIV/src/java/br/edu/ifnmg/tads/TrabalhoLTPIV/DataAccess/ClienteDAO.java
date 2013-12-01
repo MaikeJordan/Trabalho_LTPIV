ae67d159d1f2325f8bd3b23f8b2ff929cf04aa2a
Mauro <Mauro@MuroSergio>
2013-12-01 10:12:49 -0200

    Auteração CidadesDao e ClienteDao

--- TrabalhoLTPIV/src/java/br/edu/ifnmg/tads/TrabalhoLTPIV/DataAccess/ClienteDAO.java
+++ TrabalhoLTPIV/src/java/br/edu/ifnmg/tads/TrabalhoLTPIV/DataAccess/ClienteDAO.java
@@ -6,8 +6,8 @@
 
 package br.edu.ifnmg.tads.TrabalhoLTPIV.DataAccess;
 
-import br.edu.ifnmg.tads.TrabalhoLTPIV.DoMainModel.Cidades;
-import br.edu.ifnmg.tads.TrabalhoLTPIV.DoMainModel.ICidadesRepositorio;
+import br.edu.ifnmg.tads.TrabalhoLTPIV.DoMainModel.Cliente;
+import br.edu.ifnmg.tads.TrabalhoLTPIV.DoMainModel.IClienteRepositorio;
 import java.util.HashMap;
 import java.util.List;
 import javax.ejb.Stateless;
@@ -17,17 +17,17 @@ import javax.persistence.Query;
  *
  * @author Mauro
  */
-@Stateless(name = "ICidadesRepositorio")
-public  class CidadesDAO extends DAOGenerico<Cidades> implements ICidadesRepositorio {
+@Stateless(name = "IClientesRepositorio")
+public class ClienteDAO extends DAOGenerico<Cliente> implements IClienteRepositorio{
    
-    public CidadesDAO(){
-        super(Cidades.class);
+    public ClienteDAO(){
+        super(Cliente.class);
     }
     
     @Override
-    public List<Cidades> Buscar(Cidades obj) {
+    public List<Cliente> Buscar(Cliente obj) {
         // Corpo da consulta
-        String consulta = "select c from cidade c";
+        String consulta = "select c from cliente c";
 
         // A parte where da consulta
         String filtro = "";
@@ -39,23 +39,24 @@ public  class CidadesDAO extends DAOGenerico<Cidades> implements ICidadesReposit
         if (obj != null) {
             //Nome
             if (obj.getNome() != null && obj.getNome().length() > 0) {
-                filtro += " c.nome like nome ";
+                filtro += " c.nome=:nome ";
                 parametros.put("nome", obj.getNome());
             }
-            //Id
-            if (obj.getId() != null && obj.getId() > 0) {
+            
+            //Cpf
+            if (obj.getCpf() != null && obj.getCpf().length() > 0) {
                 if (filtro.length() > 0) {
                     filtro = filtro + " and ";
                 }
-                filtro += " c.id =:id";
-                parametros.put("id", obj.getId());
+                filtro += " c.cpf=:cpf ";
+                parametros.put("cpf", obj.getCpf());
             }
+          // Tipo
             
-            if (obj.getUf() != null && obj.getUf().length() > 0) {
-                filtro += " c.estado like estado ";
-                parametros.put("estado", obj.getUf());
+            if (obj.getTipo() != null && obj.getTipo().length() > 0) {
+                filtro += " c.tipo=:tipo ";
+                parametros.put("tipo", obj.getTipo());
             }
-            
             // Se houver filtros, coloca o "where" na consulta
             if (filtro.length() > 0) {
                 consulta = consulta + " where " + filtro;
@@ -76,7 +77,18 @@ public  class CidadesDAO extends DAOGenerico<Cidades> implements ICidadesReposit
     }
 
     @Override
-    public boolean Apagar(Cidades obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean Apagar(Cliente obj) {
+       try {
+            Query query = manager.createQuery("Update cliente s set s.ativo = 0 WHERE s.id :=id");
+            query.setParameter("id", obj.getId());
+            query.executeUpdate();
+
+            return true;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+            return false;
+        }
     }
 }
