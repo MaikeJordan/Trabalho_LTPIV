f9fdd1ff5d42fe2f13e5cab79905879dcc32bee1
Mauro <Mauro@MuroSergio>
2013-12-01 12:36:52 -0200

    Modificação  FrotasautomoveisDao

---
+++ TrabalhoLTPIV/src/java/br/edu/ifnmg/tads/TrabalhoLTPIV/DataAccess/FrotasautomoveisDAO.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.tads.TrabalhoLTPIV.DataAccess;
+
+import br.edu.ifnmg.tads.TrabalhoLTPIV.DoMainModel.Frotasautomoveis;
+import br.edu.ifnmg.tads.TrabalhoLTPIV.DoMainModel.IFrotasautomoveisRepositorio;
+import java.util.HashMap;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Mauro
+ */
+@Stateless(name = "IfrotasautomoveisRepositorio")
+public class FrotasautomoveisDAO extends DAOGenerico<Frotasautomoveis> implements IFrotasautomoveisRepositorio{
+    
+    public FrotasautomoveisDAO(){
+        super(Frotasautomoveis.class);
+    }
+    
+    @Override
+    public List<Frotasautomoveis> Buscar(Frotasautomoveis obj) {
+       // Corpo da consulta
+        String consulta = "select f from frotasautomoveis f";
+
+        // A parte where da consulta
+        String filtro = "";
+
+        // Guarda a lista de parâmetros da query
+        HashMap<String, Object> parametros = new HashMap<String, Object>();
+
+        // Verifica campo por campo os valores que serão filtrados
+        if (obj != null) {
+            //Nome
+            if (obj.getNome() != null && obj.getNome().length() > 0) {
+                filtro += " f.nome like nome ";
+                parametros.put("nome", obj.getNome());
+            }
+            //Id
+            if (obj.getId() != null && obj.getId() > 0) {
+                if (filtro.length() > 0) {
+                    filtro = filtro + " and ";
+                }
+                filtro += " f.id like id";
+                parametros.put("id", obj.getId());
+            }
+            
+
+            // Se houver filtros, coloca o "where" na consulta
+            if (filtro.length() > 0) {
+                consulta = consulta + " where " + filtro;
+            }
+        }
+
+        // Cria a consulta no JPA
+        Query query = manager.createQuery(consulta);
+
+        // Aplica os parâmetros da consulta
+        for (String par : parametros.keySet()) {
+            query.setParameter(par, parametros.get(par));
+        }
+
+        // Executa a consulta
+        return query.getResultList();
+
+    }
+
+    @Override
+    public boolean Apagar(Frotasautomoveis obj) {
+         try {
+
+            Query query = manager.createQuery("Update frotasautomoveis f set f.ativo = 0 WHERE f.id :=id");
+            query.setParameter("id", obj.getId());
+            query.executeUpdate();
+
+            return true;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+            return false;
+        }
+    }
+}
