8a6ac254a24c0552b6d7ebd77da1075041d83e16
Mauro <Mauro@MuroSergio>
2013-12-02 11:36:58 -0200

    Modificação  Tipoproduto Repositorio

---
+++ TrabalhoLTPIV/src/java/br/edu/ifnmg/tads/TrabalhoLTPIV/DoMainModel/ITipoprodutoRepositorio.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.tads.TrabalhoLTPIV.DoMainModel;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author Mauro
+ */
+@Remote
+public interface ITipoprodutoRepositorio extends IRepositorio<Tipoproduto>{
+    
+}
