43d0a3cb59112450cebe28370f5dba5803aa7a57
Mauro <Mauro@MuroSergio>
2013-12-02 09:05:27 -0200

    Modificação  FrotasautomoveisDao

--- TrabalhoLTPIV/src/java/br/edu/ifnmg/tads/TrabalhoLTPIV/DataAccess/FrotasautomoveisDAO.java
+++ TrabalhoLTPIV/src/java/br/edu/ifnmg/tads/TrabalhoLTPIV/DataAccess/FrotasautomoveisDAO.java
@@ -75,7 +75,7 @@ public class FrotasautomoveisDAO extends DAOGenerico<Frotasautomoveis> implement
     public boolean Apagar(Frotasautomoveis obj) {
          try {
 
-            Query query = manager.createQuery("Update frotasautomoveis f set f.ativo = 0 WHERE f.id :=id");
+            Query query = manager.createQuery("Update frotasautomoveis s set s.ativo = 0 WHERE s.id :=id");
             query.setParameter("id", obj.getId());
             query.executeUpdate();
 
