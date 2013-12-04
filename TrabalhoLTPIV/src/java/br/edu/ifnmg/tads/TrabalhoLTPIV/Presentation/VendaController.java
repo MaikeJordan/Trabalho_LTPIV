977134700e29c051ebe9d62e0b76614104208ca2
Desktop <Desktop@PC>
2013-12-04 09:28:01 -0200

    Criação da Camada Presentation

---
+++ TrabalhoLTPIV/src/java/br/edu/ifnmg/tads/TrabalhoLTPIV/Presentation/VendaController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.edu.ifnmg.tads.TrabalhoLTPIV.Presentation;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Desktop
+ */
+@Named(value = "vendaController")
+@SessionScoped
+public class VendaController implements Serializable {
+
+    /**
+     * Creates a new instance of VendaController
+     */
+    public VendaController() {
+    }
+    
+}
